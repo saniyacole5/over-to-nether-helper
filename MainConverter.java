93cf1fba6367cb6c6d7b1545937b2e039b409a70
saniyacole5 <66504277+saniyacole5@users.noreply.github.com>
2020-06-09 18:51:45 -0500

    revised  z coord converter and x coord converter
    this implements casting and the math round method from the math class

--- MainConverter.java
+++ MainConverter.java
@@ -1,33 +1,35 @@
-/// mainConverter.java
+// mainConverter.java
 // sub-classes to methods?
 // for an example:
 /* x coord converter into one method and
    z coord converter into one method?
- */   
+ */
+ 
+import java.lang.Math;
    
 public class MainConverter
 {
    public static void main(String[] args)
    {
       // Example
-      System.out.println(xCoordConverter(80) + ", yCoord, " + zCoordConverter(7689));
+      System.out.println(xCoordConverter(7695) + ", yCoord, " + zCoordConverter(7695));
       
       
       
    }
    
-   public static int xCoordConverter(int xCoord)
+   public static int xCoordConverter(double xCoord)
    {
       int convertedXCoord = 0;
-      convertedXCoord = xCoord/8;
+      convertedXCoord = (int) (Math.round(xCoord/8));
       return convertedXCoord;
        
    }
    
-   public static int zCoordConverter(int zCoord)
+   public static int zCoordConverter(double zCoord)
    {
       int convertedZCoord = 0;
-      convertedZCoord = zCoord/8;
+      convertedZCoord = (int) (Math.round(zCoord/8));
       return convertedZCoord;
    }
    
