a6913a2e265a239256e6c2397d2a178d42ab40e9
saniyacole5 <66504277+saniyacole5@users.noreply.github.com>
2020-06-15 16:06:52 -0500

    put the separate inputs in specific methods
    i put them in three diff methods. One singular method cannot return more than one value without an object or array. We could've used an object but I prefer methods. I think they're easier to use!

--- MainConverter.java
+++ MainConverter.java
@@ -10,26 +10,33 @@ import java.lang.Math;   import java.util.Scanner;
 public class MainConverter
 {
    public static void main(String[] args)
+   {    
+      System.out.print("\n\nPut your Nether Portal at these Coordinates in the Nether: " + xCoordConverter(xCoordUserInput()) + ", " + yCoordUserInput() + ", " + zCoordConverter(zCoordUserInput()));
+   }
+   
+   public static double xCoordUserInput()
    {
-      //Example
       Scanner xCoordUserInput = new Scanner(System.in);
       System.out.print("Enter X Coordinate -->  ");
       double xCoords = xCoordUserInput.nextDouble();
-      
+      return xCoords;
+   }
+   
+   public static int yCoordUserInput()
+   {
       Scanner yCoordUserInput = new Scanner(System.in);
       System.out.print("\nEnter Y Coordinate -->  ");
       double yCoord = yCoordUserInput.nextDouble();
-      int yCoords = (int) yCoord; 
-            
+      int yCoords = (int) yCoord;
+      return yCoords;
+   }
+   
+   public static double zCoordUserInput()
+   {
       Scanner zCoordUserInput = new Scanner(System.in);
       System.out.print("\nEnter Z Coordinate -->  ");
       double zCoords = zCoordUserInput.nextDouble();
-      
-      System.out.print("\n\nPut your Nether Portal at these Coordinates: " + xCoordConverter(xCoords) + ", " + yCoords + ", " + zCoordConverter(zCoords));      
-      
-      
-      
-      
+      return zCoords;
    }
    
    public static int xCoordConverter(double xCoord)
