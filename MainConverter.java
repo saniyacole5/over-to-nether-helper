08c832445a4ccd4b24d454d3560d97fa2cb91d19
saniyacole5 <66504277+saniyacole5@users.noreply.github.com>
2020-06-18 15:44:53 -0500

    polished user input!!! FINALLY!
    There will no longer be an error whenever a user puts in letters instead of numbers! If they were to input something other than a number, the program prints, "Don't be Silly! Enter a NUMBER for the __ Coordinate...", and proceeds to asking the user to input that specific coordinate again :)

--- MainConverter.java
+++ MainConverter.java
@@ -5,40 +5,92 @@
    z coord converter into one method?
  */
  
-import java.lang.Math;   import java.util.Scanner;
+import java.util.ArrayList;  import java.lang.Math;   import java.util.Scanner;
    
 public class MainConverter
 {
    public static void main(String[] args)
-   {    
-      System.out.print("\n\nPut your Nether Portal at these Coordinates in the Nether: " + xCoordConverter(xCoordUserInput()) + ", " + yCoordUserInput() + ", " + zCoordConverter(zCoordUserInput()));
+   { 
+      ArrayList<String> coordArrays = new ArrayList<>();
+      System.out.print("\n\nPut your Nether Portal at these Coordinates in the Nether: " + xCoordConverter(xCoordUserInput(coordArrays)) + ", " + yCoordUserInput(coordArrays) + ", " + zCoordConverter(zCoordUserInput(coordArrays)));
    }
    
-   public static double xCoordUserInput()
+   public static double xCoordUserInput(ArrayList<String> coordArray)
    {
+      Double xCoordNum;
       Scanner xCoordUserInput = new Scanner(System.in);
-      System.out.print("Enter X Coordinate -->  ");
-      double xCoords = xCoordUserInput.nextDouble();
-      return xCoords;
+      System.out.print("\nEnter X Coordinate -->  ");
+      String xCoord = xCoordUserInput.nextLine();
+      coordArray.add(xCoord);
+      if(isNumeric(coordArray.get(coordArray.size() - 1)))
+      {
+         xCoordNum = new Double(coordArray.get(coordArray.size() - 1));
+         return xCoordNum;
+      }
+      else
+      {
+         System.out.println("\nDon't be Silly! Enter a NUMBER for the X Coordinate...");
+         xCoordUserInput(coordArray);
+      }
+    xCoordNum = new Double(coordArray.get(coordArray.size() - 1));
+    return xCoordNum; 
    }
    
-   public static int yCoordUserInput()
+   public static double yCoordUserInput(ArrayList<String> coordArray)
    {
+      Double yCoordNum;
       Scanner yCoordUserInput = new Scanner(System.in);
       System.out.print("\nEnter Y Coordinate -->  ");
-      double yCoord = yCoordUserInput.nextDouble();
-      int yCoords = (int) yCoord;
-      return yCoords;
+      String yCoord = yCoordUserInput.nextLine();
+      coordArray.add(yCoord);
+      if(isNumeric(coordArray.get(coordArray.size() - 1)))
+      {
+         yCoordNum = new Double(coordArray.get(coordArray.size() - 1));
+         return yCoordNum;
+      }
+      else
+      {
+         System.out.println("\nDon't be Silly! Enter a NUMBER for the Y Coordinate...");
+         yCoordUserInput(coordArray);
+      }
+    yCoordNum = new Double(coordArray.get(coordArray.size() - 1));
+    return yCoordNum; 
    }
    
-   public static double zCoordUserInput()
+   public static double zCoordUserInput(ArrayList<String> coordArray)
    {
+      Double zCoordNum;
       Scanner zCoordUserInput = new Scanner(System.in);
       System.out.print("\nEnter Z Coordinate -->  ");
-      double zCoords = zCoordUserInput.nextDouble();
-      return zCoords;
+      String zCoord = zCoordUserInput.nextLine();
+      coordArray.add(zCoord);
+      if(isNumeric(coordArray.get(coordArray.size() - 1)))
+      {
+         zCoordNum = new Double(coordArray.get(coordArray.size() - 1));
+         return zCoordNum;
+      }
+      else
+      {
+         System.out.println("\nDon't be Silly! Enter a NUMBER for the Z Coordinate...");
+         zCoordUserInput(coordArray);
+      }
+    zCoordNum = new Double(coordArray.get(coordArray.size() - 1));
+    return zCoordNum; 
    }
-   
+
+   public static boolean isNumeric(String str)
+   {
+      try 
+      {  
+         Double.parseDouble(str);  
+         return true;
+      } 
+      catch(NumberFormatException e)
+      {  
+         return false;  
+      }
+   }  
+
    public static int xCoordConverter(double xCoord)
    {
       int convertedXCoord = 0;
